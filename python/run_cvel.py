242429dea82df7a88381b2729fd79bc1f24f9ade
KevinVinsen <kvn1961@gmail.com>
2014-09-22 17:21:29 +0800

    Start multiple instances from the command line

--- python/run_cvel.py
+++ python/run_cvel.py
@@ -40,17 +40,20 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)-15s:' + logging.BASIC
 LOG.info('PYTHONPATH = {0}'.format(sys.path))
 
 
-def start_servers(ami_id, user_data, instance_type, volume_id, created_by, name, spot_price=None):
+def start_servers(ami_id, user_data, instance_type, volume_ids, created_by, name, spot_price=None):
     ec2_helper = EC2Helper()
+    count = 1
+    for volume_id in volume_ids:
+        # Get the name of the volume
+        volume_name = ec2_helper.get_volume_name(volume_id)
+        user_data = get_mime_encoded_user_data(user_data, volume_name)
 
-    # Get the name of the volume
-    volume_name = ec2_helper.get_volume_name(volume_id)
-    user_data = get_mime_encoded_user_data(user_data, volume_name)
+        if spot_price is not None:
+            ec2_helper.run_spot_instance(ami_id, spot_price, user_data, instance_type, volume_id, created_by, name + ' {0}'.format(count), ephemeral=True)
+        else:
+            ec2_helper.run_instance(ami_id, user_data, instance_type, volume_id, created_by, name + ' {0}'.format(count), ephemeral=True)
 
-    if spot_price is not None:
-        ec2_helper.run_spot_instance(ami_id, spot_price, user_data, instance_type, volume_id, created_by, name, ephemeral=True)
-    else:
-        ec2_helper.run_instance(ami_id, user_data, instance_type, volume_id, created_by, name, ephemeral=True)
+        count += 1
 
 
 def get_mime_encoded_user_data(data, volume_name):
@@ -97,8 +100,8 @@ def check_args(args):
     """
     map_args = {}
 
-    if args['vol_id'] is not None:
-        map_args['vol_id'] = args['vol_id']
+    if args['vol_ids'] is not None:
+        map_args['vol_ids'] = args['vol_ids']
     else:
         return None
 
@@ -124,20 +127,20 @@ def check_args(args):
 def main():
     parser = argparse.ArgumentParser('Start a number of CVEL servers')
     parser.add_argument('-a', '--ami_id', help='the AMI id to use')
-    parser.add_argument('-v', '--vol_id', required=True, help='the volume id to use')
     parser.add_argument('-i', '--instance_type', required=True, help='the instance type to use')
     parser.add_argument('-c', '--created_by', help='the username to use')
     parser.add_argument('-n', '--name', required=True, help='the instance name to use')
     parser.add_argument('-s', '--spot_price', type=float, help='the spot price to use')
     parser.add_argument('-b', '--bash_script', help='the bash script to use')
+    parser.add_argument('vol_ids', nargs='+', help='the volume ids to use')
 
     args = vars(parser.parse_args())
 
     args1 = check_args(args)
-    if args is None:
+    if args1 is None:
         LOG.error('The arguments are incorrect: {0}'.format(args))
     else:
-        start_servers(args1['ami_id'], args1['user_data'], args1['instance_type'], args1['vol_id'], args1['created_by'], args1['name'], args1['spot_price'])
+        start_servers(args1['ami_id'], args1['user_data'], args1['instance_type'], args1['vol_ids'], args1['created_by'], args1['name'], args1['spot_price'])
 
 if __name__ == "__main__":
     main()
