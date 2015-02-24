2681ee982d8bba3e12d159ae54733fdd666236f2
KevinVinsen <kvn1961@gmail.com>
2015-02-24 14:44:05 +0800

    Getting ready to run Clean on many machines

--- python/loop_clean.py
+++ python/loop_clean.py
@@ -30,8 +30,7 @@ This module should run together with the casapy: e.g. casapy --nologger -c loop_
 import fnmatch
 import os
 from os.path import join
-from echo import dump_all
-from makecube_defines import check_dir, cube_dir, vis_dirs, run_id, do_cube, freq_min, freq_max, freq_step, freq_width, job_id, num_jobs, debug
+from makecube_defines import check_dir, cube_dir, vis_dirs, run_id, do_cube, freq_min, freq_max, freq_step, freq_width
 
 check_dir(cube_dir)
 
@@ -44,7 +43,6 @@ for root, dir_names, filenames in os.walk(vis_dirs):
     for match in fnmatch.filter(dir_names, 'vis_*'):
         vis_dirs_cube.append('{0}'.format(join(root, match)))
 
-dump_all()
 do_cube(vis_dirs_cube, cube_dir, freq_min, freq_max, freq_step, freq_width)
 
 # Done
