04c7fd483b4b5c86748f5773e5459a8d53d9c3c9
KevinVinsen <kvn1961@gmail.com>
2015-03-26 12:46:43 +0800

    New version of launch trace

--- python/launch_trace2.py
+++ python/launch_trace2.py
@@ -96,6 +96,7 @@
 
 """
 
+import pdb
 import logging
 import os
 from os import makedirs
@@ -261,6 +262,7 @@ class Trace():
 
         # Store the trace details
         hertz = int(os.sysconf(os.sysconf_names['SC_CLK_TCK']))
+        pdb.set_trace()
         self._connection.execute(
             TRACE_DETAILS.insert,
             start_time=(start_time - EPOCH).total_seconds(),
